c7424d17f289ff6b92d8aa1d11b0195555d47285
Varalakshmibyreddy <varubyreddy@gmail.com>
2024-07-21 15:26:23 +0530

    added new project

--- textbased.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('newsletter-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = document.getElementById('email').value;
-    alert(`Thank you for subscribing with ${email}!`);
-});
